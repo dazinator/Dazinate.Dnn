a3aa6344f222c531e845ba5e249b6976e4430cec
DarrellTunnell <darrell.tunnell@googlemail.com>
2017-01-13 05:46:48 +0000

    Added nuspec.

--- src/Dazinate.Dnn.Manifest/Properties/AssemblyInfo.cs
+++ src/Dazinate.Dnn.Manifest/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Dazinate.Dnn.Manifest")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A library for producing Dnn Manifests.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Dazinate.Dnn.Manifest")]
